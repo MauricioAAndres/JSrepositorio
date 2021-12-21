f78e06d505b5c9e41265ff739904313b1b224397
Mauric <mauricioalenandres@gmail.com>
2021-12-21 16:22:35 -0300

    jS

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hola");
+
+let nomentr = prompt("ingrese aqui su nombre");
+let edadentr = prompt("ingrese edad");
+let anio = 2021 - edadentr;
+let edadsali =
+  "Su nombre es " +
+  nomentr +
+  " , su edad es " +
+  edadentr +
+  " y nacio en " +
+  anio;
+
+alert(edadsali);
+console.log(edadsali);
